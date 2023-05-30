d41555f7c111d1f81a1432f63ecb392e6364a34f
Cesar Morales <cesarmorales@cardioperfusion.com>
2023-05-30 17:24:55 -0500

    Mostrando data dinamica en widget creado

---
+++ lib/ui/widget/item_pokemon_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class ItemPokemonWidget extends StatelessWidget {
+  String name;
+  String image;
+
+  ItemPokemonWidget({
+    required this.name,
+    required this.image,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xff8CC7B0),
+        borderRadius: BorderRadius.circular(18),
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: -20,
+            right: -20,
+            child: Image.network(image),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 12,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  name,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 6),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 14,
+                    vertical: 4,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.27),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.07),
+                        offset: Offset(4, 4),
+                        blurRadius: 12,
+                      ),
+                    ],
+                  ),
+                  child: Text(
+                    "Grass",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Image.network(
+              "http://www.serebii.net/pokemongo/pokemon/001.png",
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
